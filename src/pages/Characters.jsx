0d0741b0ad930ed0d3a72dad0b15ab580ed4ca85
MaksymZaluskiy <zmax1301@gmail.com>
2025-08-15 19:19:09 +0200

    proto_filter

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -1,19 +1,27 @@
 import useData from "../hooks/useData"
 import useSetData from "../hooks/useSetData"
 import Character from "../components/Character"
+import { useState, useRef } from "react"
 
 const Characters = () => {
+    const [query, setQuery] = useState("")
+    const input = useRef("")
 
     const data = useData("https://api.api-onepiece.com/v2/characters/en")
     const characters = useSetData(data)
 
-    
+    const handleChange = (e) => {
+        setQuery(e.target.value)
+    }   
+
     return(
         <>
              {characters ? (
+                <div>
+                    <input ref={input} type="text" onChange={e => handleChange(e)}/>
+                    {characters.filter(character => character.name.includes(query)).map((character) => <Character key={character.id} character={character}/>)}
+                </div>
                 
-                characters.map((character) => <Character key={character.id} character={character}/>)
-
             ) : (
                 <p>Loading...</p>
             )}
