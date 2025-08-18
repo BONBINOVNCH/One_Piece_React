e7f9269225f0400899d590549f6b11ce7eb6abec
MaksymZaluskiy <zmax1301@gmail.com>
2025-08-18 15:59:28 +0200

    main_page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,25 +2,27 @@ import { Link, Outlet } from "react-router"
 
 const Header = () => {
     return (
-        <header>
-                <nav>
-                    <img src={"img/One-Piece-Logo.png"} alt="" className="logo" />
+        <>
+         <header className="header">
+    <div className="logo">ONE PIECE</div>
+    <nav className="nav">
+      <ul>
+        <Link to="/" className="nav_item">Головна</Link>
+        <Link to="characters" className="nav_item">Персонажі</Link>
+        <Link to="fruits" className="nav_item">Фрукти</Link>
+      </ul>
+    </nav>
+   
+  </header>
+         <Outlet/>
+        </>
+
+    )
+}
                     
-                    <div className="nav_block">
+                    {/* <div className="nav_block">
                         <Link to="/" className="nav_item">Головна</Link>
                         <Link to="characters" className="nav_item">Персонажі</Link>
                         <Link to="fruits" className="nav_item">Фрукти</Link>
-                    </div>
-
-                    <div className="search_block">
-                        
-                    </div>
-
-                <Outlet/>
-            </nav>
-        </header>
-        
-    )
-}
-
+                    </div> */}
 export default Header
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,27 @@
-
+import hero from "/img/one-piece-1920x1080.jpg"
 
 const Home = () => {
-
-
-
     return(
         <>
-        <div>Home</div>
+          <section className="hero">
+            <div className="hero-img">
+                <img src={hero} alt="" />
+            </div>
+    <div className="hero-text">
+      <h2>Long ago...</h2>
+      <p>
+        The infamous Gol D. Roger was the strongest and most powerful pirate on the seas...
+        <br></br>
+        Monkey D. Luffy is a spirited, energetic and somewhat dim-witted young man with a very big dream.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae delectus neque suscipit. Incidunt natus assumenda repellat magni dolor nesciunt exercitationem, porro quas! In sed magnam sapiente dignissimos fugiat. Rerum, minima. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis illum excepturi vero architecto corporis qui officiis, voluptatem consequuntur nisi rem! Laudantium inventore nesciunt optio neque hic dignissimos nisi doloribus saepe!
+      </p>
+      <div className="info">
+        <div><strong>Studio:</strong><br /><span>Toei Animation</span></div>
+        <div><strong>Episodes:</strong><br /><span>1000+</span></div>
+        <div><strong>Year:</strong><br /><span>1999</span></div>
+      </div>
+    </div>
+  </section>
         </>
     )
 }
